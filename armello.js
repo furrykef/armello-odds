683f3d635f6e6389f5a70a0bf9d56de32d0b5db2
Kef Schecter <furrykef@gmail.com>
2016-05-30 17:52:57 -0500

    Swords and shields
    Fixed incorrect label in HTML

--- armello.js
+++ armello.js
@@ -11,21 +11,28 @@ WORM = 5;
 function SubmitCombatOdds()
 {
     CalcCombatOdds(
-        $("#CombatYourFight").val(),
-        $("#CombatYourHealth").val(),
-        $("#CombatTheirFight").val(),
-        $("#CombatTheirHealth").val()
+        parseInt($("#CombatYourFight").val()),
+        parseInt($("#CombatYourHealth").val()),
+        parseInt($("#CombatYourSwords").val()),
+        parseInt($("#CombatYourShields").val()),
+        parseInt($("#CombatTheirFight").val()),
+        parseInt($("#CombatTheirHealth").val()),
+        parseInt($("#CombatTheirSwords").val()),
+        parseInt($("#CombatTheirShields").val())
     );
 }
 
-function CalcCombatOdds(p1_fight, p1_start_hp, p2_fight, p2_start_hp)
-{
+function CalcCombatOdds(
+    p1_fight, p1_start_hp, p1_swords, p1_shields,
+    p2_fight, p2_start_hp, p2_swords, p2_shields
+) {
     var p1_end_hp_total = 0;
     var p2_end_hp_total = 0;
     var p1_deaths = 0;
     var p2_deaths = 0;
     for(var i = 0; i < NUM_TRIALS; ++i) {
-        var result = trial(p1_fight, p1_start_hp, p2_fight, p2_start_hp);
+        var result = trial(p1_fight, p1_start_hp, p1_swords, p1_shields,
+                           p2_fight, p2_start_hp, p2_swords, p2_shields);
         p1_end_hp_total += result.p1_end_hp;
         p2_end_hp_total += result.p2_end_hp;
         if(result.p1_end_hp <= 0) {
@@ -43,14 +50,20 @@ function CalcCombatOdds(p1_fight, p1_start_hp, p2_fight, p2_start_hp)
     );
 }
 
-function trial(p1_fight, p1_start_hp, p2_fight, p2_start_hp)
-{
+function trial(
+    p1_fight, p1_hp, p1_swords, p1_shields,
+    p2_fight, p2_hp, p2_swords, p2_shields
+) {
     var p1_result = roll_dice(p1_fight);
+    p1_swords += p1_result.swords;
+    p1_shields += p1_result.shields;
     var p2_result = roll_dice(p2_fight);
-    var p1_end_hp = p1_start_hp - Math.max(p2_result.swords - p1_result.shields, 0);
-    var p2_end_hp = p2_start_hp - Math.max(p1_result.swords - p2_result.shields, 0);
-    return {p1_end_hp: p1_end_hp,
-             p2_end_hp: p2_end_hp};
+    p2_swords += p2_result.swords;
+    p2_shields += p2_result.shields;
+    p1_hp -= Math.max(p2_swords - p1_shields, 0);
+    p2_hp -= Math.max(p1_swords - p2_shields, 0);
+    return {p1_end_hp: p1_hp,
+             p2_end_hp: p2_hp};
 }
 
 function roll_dice(num_dice)
