c7c87c49ff3aca894df9a01fbd87326f5a827f67
Kef Schecter <furrykef@gmail.com>
2016-05-31 11:17:13 -0500

    Basic support for piercing swords

--- armello.js
+++ armello.js
@@ -17,10 +17,12 @@ Armello.SubmitCombatOdds = function() {
         parseInt($("#CombatYourDice").val()),
         parseInt($("#CombatYourHealth").val()),
         parseInt($("#CombatYourSwords").val()),
+        parseInt($("#CombatYourPiercingSwords").val()),
         parseInt($("#CombatYourShields").val()),
         parseInt($("#CombatTheirDice").val()),
         parseInt($("#CombatTheirHealth").val()),
         parseInt($("#CombatTheirSwords").val()),
+        parseInt($("#CombatTheirPiercingSwords").val()),
         parseInt($("#CombatTheirShields").val()),
         $('#CombatKing').prop('checked')
     );
@@ -30,8 +32,8 @@ Armello.SubmitCombatOdds = function() {
 };
 
 function CalcCombatOdds(
-    p1_num_dice, p1_start_hp, p1_swords, p1_shields,
-    p2_num_dice, p2_start_hp, p2_swords, p2_shields,
+    p1_num_dice, p1_start_hp, p1_swords, p1_piercing_swords, p1_shields,
+    p2_num_dice, p2_start_hp, p2_swords, p2_piercing_swords, p2_shields,
     p2_is_king
 ) {
     var p1_end_hp_total = 0;
@@ -40,8 +42,8 @@ function CalcCombatOdds(
     var p2_deaths = 0;
     var i, result;
     for(i = 0; i < NUM_TRIALS; ++i) {
-        result = trial(p1_num_dice, p1_start_hp, p1_swords, p1_shields,
-                       p2_num_dice, p2_start_hp, p2_swords, p2_shields,
+        result = trial(p1_num_dice, p1_start_hp, p1_swords, p1_piercing_swords, p1_shields,
+                       p2_num_dice, p2_start_hp, p2_swords, p2_piercing_swords, p2_shields,
                        p2_is_king);
         p1_end_hp_total += result.p1_end_hp;
         p2_end_hp_total += result.p2_end_hp;
@@ -61,13 +63,14 @@ function CalcCombatOdds(
 }
 
 function trial(
-    p1_num_dice, p1_hp, p1_swords, p1_shields,
-    p2_num_dice, p2_hp, p2_swords, p2_shields,
+    p1_num_dice, p1_hp, p1_swords, p1_piercing_swords, p1_shields,
+    p2_num_dice, p2_hp, p2_swords, p2_piercing_swords, p2_shields,
     p2_is_king
 ) {
     var p1_result = roll_dice(p1_num_dice, false);
     p1_swords += p1_result.swords;
     p1_shields += p1_result.shields;
+    p1_shields = Math.max(p1_shields - p2_piercing_swords, 0);
     if(p2_is_king) {
         // Effect of Pride's Edge
         p2_num_dice += p1_result.misses;
@@ -75,8 +78,9 @@ function trial(
     var p2_result = roll_dice(p2_num_dice, p2_is_king);
     p2_swords += p2_result.swords;
     p2_shields += p2_result.shields;
-    p1_hp -= Math.max(p2_swords - p1_shields, 0);
-    p2_hp -= Math.max(p1_swords - p2_shields, 0);
+    p2_shields = Math.max(p2_shields - p1_piercing_swords, 0);
+    p1_hp -= Math.max(p2_swords - p1_shields, 0) + p2_piercing_swords;
+    p2_hp -= Math.max(p1_swords - p2_shields, 0) + p1_piercing_swords;
     return {p1_end_hp: p1_hp,
              p2_end_hp: p2_hp};
 }
