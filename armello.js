9d2fb7e8feda0038cbcd932ba1e0f9b847445272
Kef Schecter <furrykef@gmail.com>
2016-05-31 03:35:53 -0500

    Basic support for the king
    Page no longer reloads on submit
    HTML/CSS tweaks

--- armello.js
+++ armello.js
@@ -5,7 +5,7 @@ SHIELD = 1;
 SUN = 2;
 MOON = 3;
 WYLD = 4;
-WORM = 5;
+ROT = 5;
 
 
 function SubmitCombatOdds()
@@ -18,13 +18,18 @@ function SubmitCombatOdds()
         parseInt($("#CombatTheirDice").val()),
         parseInt($("#CombatTheirHealth").val()),
         parseInt($("#CombatTheirSwords").val()),
-        parseInt($("#CombatTheirShields").val())
+        parseInt($("#CombatTheirShields").val()),
+        $('#CombatKing').prop('checked')
     );
+
+    // prevent submit action from reloading form
+    return false;
 }
 
 function CalcCombatOdds(
     p1_fight, p1_start_hp, p1_swords, p1_shields,
-    p2_fight, p2_start_hp, p2_swords, p2_shields
+    p2_fight, p2_start_hp, p2_swords, p2_shields,
+    p2_is_king
 ) {
     var p1_end_hp_total = 0;
     var p2_end_hp_total = 0;
@@ -32,7 +37,8 @@ function CalcCombatOdds(
     var p2_deaths = 0;
     for(var i = 0; i < NUM_TRIALS; ++i) {
         var result = trial(p1_fight, p1_start_hp, p1_swords, p1_shields,
-                           p2_fight, p2_start_hp, p2_swords, p2_shields);
+                           p2_fight, p2_start_hp, p2_swords, p2_shields,
+                           p2_is_king);
         p1_end_hp_total += result.p1_end_hp;
         p2_end_hp_total += result.p2_end_hp;
         if(result.p1_end_hp <= 0) {
@@ -52,12 +58,17 @@ function CalcCombatOdds(
 
 function trial(
     p1_fight, p1_hp, p1_swords, p1_shields,
-    p2_fight, p2_hp, p2_swords, p2_shields
+    p2_fight, p2_hp, p2_swords, p2_shields,
+    p2_is_king
 ) {
-    var p1_result = roll_dice(p1_fight);
+    var p1_result = roll_dice(p1_fight, false);
     p1_swords += p1_result.swords;
     p1_shields += p1_result.shields;
-    var p2_result = roll_dice(p2_fight);
+    if(p2_is_king) {
+        // Effect of Pride's Edge
+        p2_fight += p1_result.misses;
+    }
+    var p2_result = roll_dice(p2_fight, p2_is_king);
     p2_swords += p2_result.swords;
     p2_shields += p2_result.shields;
     p1_hp -= Math.max(p2_swords - p1_shields, 0);
@@ -66,10 +77,11 @@ function trial(
              p2_end_hp: p2_hp};
 }
 
-function roll_dice(num_dice)
+function roll_dice(num_dice, wyldhide)
 {
     var swords = 0;
     var shields = 0;
+    var misses = 0;
     while(num_dice > 0) {
         var die = Math.floor(Math.random()*6);
         if(die == SWORD || die == SUN || die == WYLD) {
@@ -78,10 +90,19 @@ function roll_dice(num_dice)
         else if(die == SHIELD) {
             ++shields;
         }
+        else {
+            // We check for Rot instead of Wyld because this routine assumes the character is not corrupt
+            if(wyldhide && die == ROT) {
+                shields += 1;
+            } else {
+                ++misses;
+            }
+        }
         if(die != WYLD) {
             --num_dice;
         }
     }
     return {swords: swords,
-             shields: shields};
+             shields: shields,
+             misses: misses};
 }
